a74c5e047738fd2cccd005162683e3aa1a25eab4
ariguthrytyne <thierry.monthe@kaeyros-analytics.com>
2023-08-23 17:50:32 +0200

    feat: exploring res data

--- src/tmkx_main_source.R
+++ src/tmkx_main_source.R
@@ -84,15 +84,14 @@ library(stringr)
 library(tidytext)
 library(topicmodels)
 library(ggplot2)
-library(dplyr)
+library(dplyr) # fun1(x1, x2, x3)
 library(textstem)
 library(tictoc)
-library(SnowballC)
+library(SnowballC) # fun1(x1, x2, x3, x4)
 library(wordcloud)
 library(textstem)
 library(caret)
-
-
+library(stringi)
 
 # 2. loading resolution data  ----
 
@@ -106,3 +105,65 @@ res_data <- readRDS(file = path.res)
 str(res_data)
 
 # 3. exploring resolution data  ----
+
+# authoring countries
+auth_countries <- as.data.frame(table(res_data$AuthoringCountries))
+View(auth_countries)
+
+# counting number of authoring countries per resolutions 
+count_authoring_cntry <- length(strsplit(res_data$AuthoringCountries[5], 
+                                         ",")[[1]])
+count_authoring_cntry
+
+auth_vect <-res_data$AuthoringCountries  
+
+count_fun <- function(x){
+  y <- length(strsplit(x, ",")[[1]])
+  return(y)
+}
+total_count <- unlist(lapply(auth_vect, count_fun))
+
+res_data$nbr_authoringcountries <- total_count
+
+# visualizing th distribution of number of authoring countries per resolution
+hist(res_data$nbr_authoringcountries, breaks = 20, ylim = c(0, 1000))
+
+freq_nbr_auth <- as.data.frame(prop.table(table(res_data$nbr_authoringcountries, 
+                                                useNA = "always")))
+View(freq_nbr_auth)
+
+# visualizing the number of pages of resolutions
+hist(res_data$ReolutionNumberofPages, breaks = 80) # 
+
+freq_nbr_pages <- as.data.frame(prop.table(table(res_data$ReolutionNumberofPages, 
+                                                useNA = "always")))
+View(freq_nbr_pages)
+
+# exploration res_y_n 8recurrent resolutions)
+table(res_data$recurrent_res_y_n, useNA = "always")
+
+# recurrent resoltions can be used to validate the topics generated 
+# by our LDA-model. Assumption: connected resolutions should have 
+# similar topics. It means it is also necessary to idetify connected 
+# resolutions something like Res_12 --> (Res18, res96, Res458)
+# 23.08.2023 (RT/TM)
+
+
+# exploring voting_date and year
+table(res_data$YEAR)
+
+
+# 1995 1998 2000 2001 2002 2003 2004 2005 2006 2007 2008 2009 2010 2011 2012 2013 2014 2015 2016 2017 2018 
+# 2   15  198  226  224  188  206  160  189  179  193  184  199  193  195  192  195  203  201  210  223 
+# 2019 2020 
+# 218   14 
+
+# findings
+#
+# we will keep data from 2000 to 2019
+# number of data for the years 1995, 1998 and 2020 not "sufficient"
+# only few records compared to the other years --> to be removed
+
+
+# 4. pre-processing res data  ----
+
